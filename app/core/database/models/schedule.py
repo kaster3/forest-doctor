4248a087ddbcb836c513bad6560dbcee6cdf4af0
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-19 02:58:13 +0300

    create schedule alchemy model

---
+++ app/core/database/models/schedule.py
@@ -0,0 +1,19 @@
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.core.database.models.base import Base
+from app.core.database.models.mixins import IntIdPkMixin
+from app.core.database.models.user import User
+
+
+class Schedule(IntIdPkMixin, Base):
+
+    drug_name: Mapped[str] = mapped_column(String(30))
+    taking_per_day: Mapped[int]
+    duration: Mapped[int]
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id", on_delete="CASCADE"),
+    )
+
+    # Связь с таблицей User (многие к одному)
+    user: Mapped[User] = relationship(back_populates="schedules")
