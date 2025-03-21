bf241fb4418f9504be5244569cb22ba5d8cd6d31
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-22 02:08:18 +0300

    add drug router

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -1,16 +1,15 @@
 from fastapi import APIRouter
 
+from app.api.api_v1.drugs.handlers import router as drug_router
 from app.api.api_v1.schedules.handlers import router as schedule_router
 from app.api.api_v1.users.handlers import router as user_router
 from app.core import settings
-
-from .some_endpoint import router as endpoint
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
 
-for rout in (endpoint, user_router, schedule_router):
+for rout in (user_router, schedule_router, drug_router):
     router.include_router(
         router=rout,
     )
