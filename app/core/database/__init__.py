2ede47ba665a7cc76d30f9bf2f0139f78cbf4ecf
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-22 02:09:22 +0300

    add classes to vision of alembic

---
+++ app/core/database/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    "User",
+    "Schedule",
+    "Drug",
+)
+
+from app.core.database.models.drug import Drug
+from app.core.database.models.schedule import Schedule
+from app.core.database.models.user import User
