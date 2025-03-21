43a3705503b43a87de9aa5fe0e0a6140cc2dc28f
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-22 02:10:00 +0300

    create drug repository and interface

---
+++ app/core/repositories/drug.py
@@ -0,0 +1,40 @@
+from abc import abstractmethod
+from typing import Protocol
+
+from sqlalchemy.exc import IntegrityError
+
+from app.core.database import Drug
+from app.core.exceptions import DrugAlreadyExistsError
+from app.core.repositories.base import Repository
+
+
+class DrugRepository(Protocol):
+    @abstractmethod
+    async def create(self, name: str) -> Drug | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_by_name(self, name: str) -> Drug | None:
+        raise NotImplementedError
+
+
+class IDrugRepository(Repository):
+    """
+    Класс, реализующий интерфейс DrugRepository, для работы с Drug моделью,
+    наследуем init от Repository для получения сессии
+    """
+
+    async def create(self, name: str) -> Drug | None:
+        drug = Drug(name=name)
+        self.session.add(drug)
+
+        try:
+            await self.session.commit()
+        except IntegrityError:
+            await self.session.rollback()
+            raise DrugAlreadyExistsError(f"Drug with name {name} already exists")
+        return drug
+
+    async def get_by_name(self, name: str) -> Drug | None:
+        drug = await self.session.get(Drug, name)
+        return drug
