b6865c4a04a7a8469d20e42d88242aea45a9b9ba
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-23 22:12:19 +0300

    add new interactor to find user by policy

--- app/core/use_cases/users.py
+++ app/core/use_cases/users.py
@@ -4,26 +4,37 @@ from fastapi import HTTPException, status
 
 from app.api.api_v1.users.schemas import UserCreate
 from app.core.database import User
+from app.core.exceptions import UserAlreadyExistsError
 from app.core.repositories.users import UserRepository
 
 log = logging.getLogger(__name__)
 
 
-class CreateUserInteractor:
+class BaseUserInteractor:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
 
-    async def __call__(self, user_in: UserCreate) -> User:
-        self._check_policy(user_in.policy)
-        user = await self.user_repository.create(policy=user_in.policy)
-        log.info("Patient with policy №% s created successfully" % user.policy)
-        return user
 
-    @staticmethod
-    def _check_policy(policy: int) -> None:
-        policy_str = str(policy)
-        if (quantity_digits := len(policy_str)) != 16:
+class CreateUserInteractor(BaseUserInteractor):
+    async def __call__(self, user_in: UserCreate) -> User:
+        try:
+            user = await self.user_repository.create(policy=user_in.policy)
+            log.info("Patient with policy №% s created successfully" % user.policy)
+            return user
+
+        except UserAlreadyExistsError as error:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Number of policy have to contains 16 digits, gotten {quantity_digits}",
+                detail={"message": str(error)},
             )
+
+
+class GetUserByPolicyInteractor(BaseUserInteractor):
+    async def __call__(self, policy: int) -> User | None:
+        user = await self.user_repository.get_by_policy(policy=policy)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Patient with policy № {policy} not found",
+            )
+        return user
