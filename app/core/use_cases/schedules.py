2caba6ee32c5bdc0fd73022189a2e2d4d320ecac
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-23 22:19:38 +0300

    add interactors

--- app/core/use_cases/schedules.py
+++ app/core/use_cases/schedules.py
@@ -4,31 +4,69 @@ from fastapi import HTTPException, status
 
 from app.api.api_v1.schedules.schemas import ScheduleCreateRequest
 from app.core.database.models.schedule import Schedule
+from app.core.exceptions import DrugNotFoundError, UserNotFoundError
 from app.core.repositories.schedule import ScheduleRepository
-from app.core.repositories.users import UserRepository
+from app.core.use_cases.users import GetUserByPolicyInteractor
 
 log = logging.getLogger(__name__)
 
 
 class BaseScheduleInterator:
-    def __init__(self, schedule_repository: ScheduleRepository) -> None:
+    def __init__(
+        self,
+        schedule_repository: ScheduleRepository,
+    ) -> None:
         self.schedule_repository = schedule_repository
 
 
-class CreateScheduleInterator(BaseScheduleInterator):
+class GetUserScheduleInterator(BaseScheduleInterator):
     def __init__(
-        self, schedule_repository: ScheduleRepository, user_repository: UserRepository
+        self,
+        schedule_repository: ScheduleRepository,
+        get_user_interactor: GetUserByPolicyInteractor,
     ) -> None:
-        super().__init__(schedule_repository=schedule_repository)
-        self.user_repository = user_repository
+        super().__init__(schedule_repository)
+        self.get_user_interactor = get_user_interactor
 
+
+class CreateScheduleInterator(BaseScheduleInterator):
     async def __call__(self, schedule_in: ScheduleCreateRequest) -> Schedule:
-        user = await self.user_repository.get_by_policy(policy=schedule_in.user_id)
-        if not user:
+        try:
+            schedule = await self.schedule_repository.create(schedule_in=schedule_in)
+        except DrugNotFoundError:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Patient with policy № �� {schedule_in.user_id} not found",
+                detail={"message": f"Drug '{schedule_in.drug_name}' not found"},
             )
-        schedule = await self.schedule_repository.create(schedule_in=schedule_in)
+        except UserNotFoundError:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={"message": f"User with policy '{schedule_in.user_id}' not found"},
+            )
+
         log.info("Schedule with patient's policy № %s created successfully" % schedule.user_id)
+        return schedule
+
+
+class GetSchedulesByPolicy(GetUserScheduleInterator):
+    async def __call__(self, user_policy: int) -> list[int]:
+        # я понимаю, что использую лишний запрос, но я не знаю, как по другому
+        # сообщить пользователю, что юзера вообще нет
+        await self.get_user_interactor(user_policy)
+        schedules = await self.schedule_repository.get_all_by_policy(policy=user_policy)
+        schedules_id = [schedules.id for schedules in schedules]
+        return schedules_id
+
+
+class GetScheduleByIdsInteractor(BaseScheduleInterator):
+    async def __call__(self, user_policy: int, schedule_id: int) -> Schedule | None:
+        schedule = await self.schedule_repository.get_by_ids(
+            schedule_id=schedule_id,
+            user_policy=user_policy,
+        )
+        if not schedule:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Schedule № {schedule_id} not found for patient with policy {user_policy}",
+            )
         return schedule
