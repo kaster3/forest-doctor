5ccb68f38c28ad518e19851af1c1f3c2154eb9b6
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-23 22:13:03 +0300

    create annotated to use it in another file

--- app/api/api_v1/users/schemas.py
+++ app/api/api_v1/users/schemas.py
@@ -1,8 +1,12 @@
-from pydantic import BaseModel
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+
+UserPolicy = Annotated[int, Field(ge=1000000000000000, le=9999999999999999)]
 
 
 class UserBase(BaseModel):
-    policy: int  # constr(min_length=16, max_length=16)
+    policy: UserPolicy
 
 
 class UserCreate(UserBase):
