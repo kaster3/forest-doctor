df1960e587742e18647b1d393e9eacbce429a183
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-23 22:08:20 +0300

    delete handler of getting drugs

--- app/api/api_v1/users/handlers.py
+++ app/api/api_v1/users/handlers.py
@@ -14,14 +14,8 @@ router = APIRouter(prefix="/users", tags=["users"])
     status_code=status.HTTP_201_CREATED,
 )
 @inject
-async def create_user(user_in: UserCreate, interactor: FromDishka[CreateUserInteractor]):
-    user = await interactor(user_in=user_in)
-    return user
-
-
-# http://localhost:8000/api/v1/users/next_takings?user_id=
-@router.get("/next_takings/{user_id}")
-async def get_next_takings(
-    user_id: int,
+async def create_user(
+    user_in: UserCreate,
+    interactor: FromDishka[CreateUserInteractor],
 ):
-    pass
+    return await interactor(user_in=user_in)
