63cf9f5f4019de554128e2c0af33a6d0baa12cea
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-23 22:10:11 +0300

    add logic to handle unique patients exception

--- app/core/repositories/users.py
+++ app/core/repositories/users.py
@@ -1,10 +1,11 @@
 from abc import abstractmethod
 from typing import Protocol
 
-from fastapi import HTTPException, status
+from asyncpg import UniqueViolationError
 from sqlalchemy.exc import IntegrityError
 
 from app.core.database import User
+from app.core.exceptions import UserAlreadyExistsError
 from app.core.repositories.base import Repository
 
 
@@ -24,20 +25,14 @@ class IUserRepository(Repository):
     наследуем init от Repository для получения сессии
     """
 
-    async def create(self, policy) -> User | None:
+    async def create(self, policy: int) -> User | None:
+        user = User(policy=policy)
+        self.session.add(user)
         try:
-            user = User(policy=policy)
-            self.session.add(user)
             await self.session.commit()
-            await self.session.refresh(user)
-
-        except IntegrityError:
-            await self.session.rollback()
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail={"message": f"Patient with policy № {policy} already exists"},
-            )
-
+        except IntegrityError as error:
+            if isinstance(error.orig.__cause__, UniqueViolationError):
+                raise UserAlreadyExistsError(f"Patient with policy № {policy} already exists")
         return user
 
     async def get_by_policy(self, policy: int) -> User | None:
