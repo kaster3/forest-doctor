c041b6dd19657debcaae581f335897782f2b1b20
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-23 22:00:07 +0300

    delete example tests package

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -13,8 +13,3 @@ for rout in (user_router, schedule_router, drug_router):
     router.include_router(
         router=rout,
     )
-
-
-@router.get("")
-async def root():
-    return {"message": "this path is http://127.0.0.1:8000/api/v1"}
