32f02d6016d7783295a30f83ac4583256c804670
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-23 22:11:45 +0300

    add relationship

--- app/core/database/models/user.py
+++ app/core/database/models/user.py
@@ -1,11 +1,18 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import BigInteger, CheckConstraint
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core.database.models.base import Base
+
+if TYPE_CHECKING:
+    from app.core.database.models.schedule import Schedule
 
 
 class User(Base):
     policy: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    # Связь
+    schedules: Mapped[list["Schedule"]] = relationship(back_populates="user")
 
     # Решил добавить проверку валидности полиса (id) на уровне бд, включительно
     __table_args__ = (
