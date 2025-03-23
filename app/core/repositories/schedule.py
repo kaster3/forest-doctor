07e273b6461e5fd2d068d22f15a477e98135d30f
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-03-23 22:17:20 +0300

    create of realisation of interface ScheduleRepository

--- app/core/repositories/schedule.py
+++ app/core/repositories/schedule.py
@@ -1,17 +1,22 @@
 from abc import abstractmethod
-from typing import Protocol
+from typing import TYPE_CHECKING, Protocol
 
+from asyncpg import ForeignKeyViolationError
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 
-from app.api.api_v1.schedules.schemas import ScheduleCreateRequest
 from app.core.database.models.schedule import Schedule
+from app.core.exceptions import DrugNotFoundError, UserNotFoundError
 from app.core.repositories.base import Repository
+
+if TYPE_CHECKING:
+    from app.api.api_v1.schedules.schemas import ScheduleCreateRequest
 
 
 class ScheduleRepository(Protocol):
 
     @abstractmethod
-    async def create(self, schedule_in: ScheduleCreateRequest) -> Schedule:
+    async def create(self, schedule_in: "ScheduleCreateRequest") -> Schedule:
         raise NotImplementedError
 
     @abstractmethod
@@ -23,7 +28,7 @@ class ScheduleRepository(Protocol):
         raise NotImplementedError
 
     @abstractmethod
-    async def get_by_id(self, schedule_id: int, police: int) -> list[Schedule]:
+    async def get_by_ids(self, user_policy: int, schedule_id: int) -> Schedule | None:
         """
         Возвращает данные о выбранном расписании с рассчитанным графиком приёмов на день,
         на основе id расписания и полиса пациента
@@ -37,17 +42,33 @@ class IScheduleRepository(Repository):
     наследуем init от Repository для получения сессии
     """
 
-    async def create(self, schedule_in: ScheduleCreateRequest) -> Schedule:
-        schedule = Schedule(**schedule_in.model_dump())
-        self.session.add(schedule)
-        await self.session.commit()
-        await self.session.refresh(schedule)
-        return schedule
+    async def create(self, schedule_in: "ScheduleCreateRequest") -> Schedule:
+        try:
+
+            schedule = Schedule(**schedule_in.model_dump())
+            self.session.add(schedule)
+            await self.session.commit()
+            await self.session.refresh(schedule)
+            return schedule
+
+        except IntegrityError as error:
+            error_message = str(error.orig).lower()
+            if isinstance(error.orig.__cause__, ForeignKeyViolationError):
+                if "drugs" in error_message:
+                    raise DrugNotFoundError(schedule_in.drug_name) from error
+                elif "users" in error_message:
+                    raise UserNotFoundError(schedule_in.user_id) from error
 
     async def get_all_by_policy(self, policy: int) -> list[Schedule]:
         stmt = select(Schedule).where(Schedule.user_id == policy)
         schedule = await self.session.scalars(stmt)
         return list(schedule)
 
-    async def get_by_id(self, schedule_id: int, policy: int) -> list[Schedule]:
-        pass
+    async def get_by_ids(
+        self,
+        user_policy: int,
+        schedule_id: int,
+    ) -> Schedule | None:
+        stmt = select(Schedule).where(Schedule.id == schedule_id, Schedule.user_id == user_policy)
+        schedule = await self.session.scalar(stmt)
+        return schedule
